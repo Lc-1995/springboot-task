ff79f6e721d8411c52b5913a6e23eda0c0dd797b
Lc-1995 <921690132@qq.com>
2018-12-04 15:22:32 +0800

    1

--- src/test/java/com/sakura/springboottask/SpringbootTaskApplicationTests.java
+++ src/test/java/com/sakura/springboottask/SpringbootTaskApplicationTests.java
@@ -2,12 +2,9 @@ package com.sakura.springboottask;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,7 +12,6 @@ import javax.annotation.Resource;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.File;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @RunWith(SpringRunner.class)
